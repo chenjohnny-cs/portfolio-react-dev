2714ae248c442b54c99dd601fffe123cfeee7939
Johnny Chen <78446260+chenjohnny-cs@users.noreply.github.com>
2024-09-25 16:20:55 -0400

    [Refactor] Removed unused imports.
    Removed imports that were unused.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
 function App() {
   return (
     <main>
       <h1>Welcome to my Resume Page!</h1>
       <div>
-        This webpage is currently under development.
-        This wepage is made through Vite, React and TypeScript.
-        Check out the latest commits through https://github.com/chenjohnny-cs
-        <br />
+            This webpage is currently under development.
+            <br />
+            This wepage is made through Vite, React and TypeScript.
+            Check out the latest commits through https://github.com/chenjohnny-cs
       </div>
     </main>
   );
